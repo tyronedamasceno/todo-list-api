8790391db0531cdd1601be47b1df5540fd657ba0
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-07-08 22:15:14 -0300

    Route get all tasks

--- my_todo_list/tasks/models.py
+++ my_todo_list/tasks/models.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-from sqlalchemy_utils.types.choice import ChoiceType
-
 from my_todo_list import db
 
 
@@ -10,12 +8,6 @@ class Task(db.Model):
     Model to represent a task at todo-list. A task has a title, a status
     and possibly a description
     """
-    status_choices = [
-        ('waiting', 'Waiting'),
-        ('in_progress', 'In Progress'),
-        ('done', 'Done'),
-        ('declined', 'Declined')
-    ]
     id = db.Column(
         db.Integer,
         primary_key=True
@@ -30,7 +22,7 @@ class Task(db.Model):
         default=datetime.utcnow  # give the function as argument
     )
     status = db.Column(
-        ChoiceType(status_choices),
+        db.Integer,
         nullable=False,
         default=0
     )
@@ -41,3 +33,12 @@ class Task(db.Model):
 
     def __repr__(self):
         return f'Task({self.title}, {self.status})'
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title,
+            date_posted=self.date_posted.isoformat(),
+            status=self.status,
+            description=self.description if self.description else ''
+        )
--- my_todo_list/tasks/routes.py
+++ my_todo_list/tasks/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from my_todo_list.tasks.models import Task
 
 tasks = Blueprint('tasks', __name__)
 
 
 @tasks.route('/')
-def index():
-    return 'Running app'
+@tasks.route('/tasks', methods=['GET'])
+def get_all_tasks():
+    all_tasks = Task.query.all()
+    return jsonify(status=200, data=[task.to_dict() for task in all_tasks])
