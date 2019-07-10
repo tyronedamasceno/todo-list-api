183fe370e71f069d25af567c9c761f7b17846086
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-07-10 08:10:12 -0300

    Add task items locally

--- todo_list_web/src/TodoList.js
+++ todo_list_web/src/TodoList.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e) {
+        if (this._inputElement.value !== "") {
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+        }
+
+        this._inputElement.value = "";
+        console.log(this.state.items);
+
+        e.preventDefault();
+    }
+
     render() {
         return (
             <div className="todoListMain">
               <div className="header">
-                <form>
-                  <input placeholder="enter task"></input>
+                <form onSubmit={this.addItem}>
+                  <input ref={(a) => this._inputElement = a}
+                         placeholder="enter task">
+                  </input>
                   <button type="submit">add</button>
                 </form>
               </div>
@@ -16,3 +48,4 @@ class TodoList extends Component {
 }
 
 export default TodoList;
+ 
