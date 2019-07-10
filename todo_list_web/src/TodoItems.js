c582a8d009e8da65f7e79c749c5a22cd4980e81b
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-07-10 19:53:59 -0300

    stylizing transitions

--- todo_list_web/src/TodoItems.js
+++ todo_list_web/src/TodoItems.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
 
 class TodoItems extends Component {
     constructor(props){
@@ -22,7 +23,9 @@ class TodoItems extends Component {
 
         return (
             <ul className= "theList">
-              {listItems}
+              <FlipMove duration={250} easing="ease-out">
+                {listItems}
+              </FlipMove>
             </ul>
         );
     }
