bc479942aeb8f0bc392bf5d4aae07123e5bbb114
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-07-11 21:11:32 -0300

    Insert new task to DB

--- todo_list_web/src/TodoList.js
+++ todo_list_web/src/TodoList.js
@@ -24,21 +24,29 @@ class TodoList extends Component {
     }
 
     addItem(e) {
-        if (this._inputElement.value !== "") {
+        if (this._inputElement.value.trim() !== "") {
             var newItem = {
-                text: this._inputElement.value,
-                key: Date.now()
+                title: this._inputElement.value
             };
-
-            this.setState((prevState) => {
-                return {
-                    items: prevState.items.concat(newItem)
-                };
+            fetch(TodoList.baseUrl+'/tasks', {
+                headers: {'Content-Type': 'application/json'},
+                method: 'post',
+                body: JSON.stringify(newItem)
+              }).then((response) => {
+                return response.json();
+            }).then((response) => {
+                newItem.key = response.data.id;
+                newItem.status = response.data.status;
+                this.setState((prevState) => {
+                    return {
+                        items: prevState.items.concat(newItem)
+                    };
+                });
             });
+
         }
 
         this._inputElement.value = "";
-        console.log(this.state.items);
 
         e.preventDefault();
     }
